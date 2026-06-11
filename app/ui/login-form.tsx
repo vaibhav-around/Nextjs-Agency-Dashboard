f8217dd007425b2ced082250035437e5b4dffbde
= <vaibhav.goswami.cse@gmail.com>
2026-06-11 12:53:41 +0530

    changes till step 5

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,4 +1,4 @@
-import { lusitana } from '@/app/ui/fonts';
+import { lusitana } from '@/app/ui/fonts'
 import {
   AtSymbolIcon,
   KeyIcon,
@@ -51,7 +51,7 @@ export default function LoginForm() {
                 required
                 minLength={6}
               />
-              <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-4.5 w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
         </div>
